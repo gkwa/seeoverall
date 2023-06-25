098af1d5ea98f31d3c02b66a6388ad96a402633e
Taylor Monacelli <taylormonacelli@gmail.com>
2023-06-25 11:13:36 -0400

    Working

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ for i, table in enumerate(tables):
 
 json_data = [df.to_json(orient="records", indent=2) for df in dataframes]
 
-for i, json_str in enumerate(json_data):
-    with open(f"table_{i+1}.json", "w") as file:
+for i, json_str in enumerate(json_data, 1):
+    with open(f"table_{i}.json", "w") as file:
         file.write(json_str)
-        print(f"Table {i+1} JSON data written to file.")
+        print(f"Table {i} JSON data written to file.")
